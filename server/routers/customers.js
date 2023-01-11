ff4bd6a7ba91101ad54859ee745518ebb03d1592
kaballl <76085266+kaballl@users.noreply.github.com>
2023-01-11 13:03:17 +0700

    update ui + api
    update

--- server/routers/customers.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { addCus,getCus } from "../controllers/customer.js";
-const router=express.Router();
-router.post('/',addCus);
-router.get('/',getCus);
-export default router;
--- server/routers/presentations.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { addPresentation,getPresentation,deletePresentation } from "../controllers/presentation.js";
-const router=express.Router();
-router.post('/add',addPresentation);
-router.post('/delete',deletePresentation);
-router.get('/',getPresentation);
-export default router;
