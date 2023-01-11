b92a4b2499e1cdabe38b2b99c7833f9920a7cd6f
kaballl <76085266+kaballl@users.noreply.github.com>
2023-01-11 21:56:48 +0700

    complete
    daily update

--- server/routers/customers.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addCus,getCus,logCus } from "../controllers/customer.js";
-const router=express.Router();
-router.post('/login',logCus);
-router.post('/',addCus);
-
-router.get('/',getCus);
-export default router;
--- server/routers/sliders.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getSliders } from "../controllers/slider.js";
-const router=express.Router();
-router.get('/',getSliders);
-
-export default router;
