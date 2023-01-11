c4129f4fc6bfda216ebe919cda23251f59b42648
xol60 <xold59@gmail.com>
2023-01-11 21:13:52 +0700

    up

--- client/src/components/GroupList/Group/index.js
+++ client/src/components/GroupList/Group/index.js
@@ -11,21 +11,32 @@ import {
   IconButton,
   Typography,
 } from '@mui/material';
-import MoreVertIcon from '@mui/icons-material/MoreVert'
-import FavoriteIcon from '@mui/icons-material/Favorite';
-
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import { useDispatch } from 'react-redux';
+
 import { useNavigate } from "react-router-dom";
+import { deleteGroup } from '../../../redux/actions';
 
 export default function Group({ group }) {
   
-  
+  const [data, setData] = React.useState({
+    _id:'',
+    
+  });
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-
+  const a=true;
   const goToClass = () => {
     navigate(`/${group._id}`);
   };
+  const onDelete = React.useCallback(() => {
+   
+    data._id=group._id
+    
+    dispatch(deleteGroup.deleteGroupRequest(data));
+    
+  }, [data, dispatch]);
 
   
 
@@ -35,32 +46,36 @@ export default function Group({ group }) {
       <CardHeader style={{backgroundColor: "#106de7"}}
         avatar={<Avatar>G</Avatar>}
         title={<Link underline='hover' color='white'  onClick={goToClass} variant='h4' >{group.name}</Link>}
+        action=
+        {a?(
+        <>
+        {
+          <IconButton onClick={onDelete}>
+            <DeleteIcon></DeleteIcon>
+          </IconButton>
+        }
+        </>
+      ):null}
        
         
       />
       
       
-      <CardMedia
-        image={group.avatar || ''}
+      
+      <CardContent style={{backgroundColor:'greenyellow'}}>
+        <Typography variant='h5' color='textPrimary'>
+          {group.title}
+        </Typography>
+        <Typography variant='body2' component='p' color='textSecondary'>
+          
+        </Typography>
+      </CardContent>
+      
+      <CardMedia style={{height:500}}
+        image="https://img.freepik.com/free-vector/group-therapy-illustration-concept_52683-45727.jpg?w=1060&t=st=1673414932~exp=1673415532~hmac=934f4b9aa97c52229bc072d693de2976287d6ff8ec12455f8c2ae83ea09dc6e9"
         title='Title'
         
       />
-      <CardContent>
-        <Typography variant='h5' color='textPrimary'>
-          {group.password}
-        </Typography>
-        <Typography variant='body2' component='p' color='textSecondary'>
-          {group.email}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <IconButton >
-          
-          <Typography component='span' color='textSecondary'>
-          
-          </Typography>
-        </IconButton>
-      </CardActions>
     </Card>
   );
 }
