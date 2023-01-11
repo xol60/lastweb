9f61602156e9df4cf30b58a8373080997695cb7b
kaballl <76085266+kaballl@users.noreply.github.com>
2023-01-12 06:02:29 +0700

    fianl update

--- server/routers/groups.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { addGroup } from "../controllers/group.js";
-const router=express.Router();
-router.post('/',addGroup);
-
-export default router;
--- server/routers/members.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getMembers,addMember} from "../controllers/members.js";
-const router=express.Router();
-
-router.post('/',getMembers);
-router.post('/add',addMember)
-export default router;
